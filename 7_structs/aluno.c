097a37c7c13771b97d6dd399b2830cfe146b521a
Daniel Bezerra <daniel.bezerra92@gmail.com>
2026-04-02 12:15:43 -0300

    exercicio aluno

---
+++ 7_structs/aluno.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+typedef struct Endereco
+{
+    char rua[50];
+    int numero;
+}Endereco;
+
+typedef struct aluno
+{
+    char* nome;
+    int idade;
+    Endereco end;
+}Aluno;
+
+int main(){
+    Aluno vetor[2];
+    for (int i = 0; i < 2; i++)
+    {
+        printf("Digite o nome do aluno \n");
+        scanf("%s", vetor[i].nome);
+        printf("Digite a idade do aluno \n");
+        scanf("%d", &vetor[i].idade);
+        printf("Digite o endereco do aluno \n");
+        scanf("%s", vetor[i].end.rua);
+        printf("Digite o numero do endereco do aluno \n");
+        scanf("%d", &vetor[i].end.numero);
+    }
+    for (int i = 0; i < 2; i++)
+    {
+        printf("\nAluno %d:\n", i);
+        printf("%s ", vetor[i].nome);
+        printf("\n%d ", vetor[i].idade);
+        printf("\n%s ,", vetor[i].end.rua);
+        printf("%d", vetor[i].end.numero);
+    }
+    
+}
